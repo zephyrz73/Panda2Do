ba785d449f506c1e26d3b66161eec0a317198d8a
Zoeyzyzyzy <zeyuwang@ucsb.edu>
2022-10-26 18:50:38 -0700

    add initial post list & post page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   root "posts#index"
+
+  # get '/posts', to:'posts#show'
+  # get "/posts/:id", to: "posts#post"
+
+  resources :posts do
+    resources :comments
+  end
 end
