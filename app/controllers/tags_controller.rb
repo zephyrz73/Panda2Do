22ddb038478eab868a681cd7bdc566817b8301ce
zephyrz73 <xzhou1551@outlook.com>
2022-11-04 00:18:11 -0700

    navbar with login

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,8 +2,18 @@ class TagsController < ApplicationController
   def index
     @tags = Tag.all
   end
+
   def show
     @tag = Tag.find(params[:id])
     @posts = Post.where(user: params[:id]).all
+  end
+
+  def create
+    @tag = Tag.new(name: params[:name], description: params[:description])
+    if @tag.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 end
