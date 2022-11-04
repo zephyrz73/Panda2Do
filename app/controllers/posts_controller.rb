2a74ed03d36574e41bf0aaee72e836b4c9726bfa
Zoeyzyzyzy <zeyuwang@ucsb.edu>
2022-11-04 01:24:09 -0700

    add create post & comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,32 @@ class PostsController < ApplicationController
   def show 
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    @post.user_id = current_user.id
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :body, :tag_id, :points2view, :user_id)  
+    end
+    
 end
